e7a81c14a2698ed922b633b3e678d89e731f6ccb
Roberto Aguero <AgueroR@jbu.edu>
2024-11-07 10:13:06 -0600

    Test commit

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world!"
 
+@app.route("/GET")
+def get_data():
+    return "Succesfully got data.."
+
 if __name__ == "__main__":
     app.run(debug=True)
